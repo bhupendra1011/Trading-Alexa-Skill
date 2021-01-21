cbb1350cbc0545f3637fc8dfdad863cf505f1022
Bhupendra <bhupendra1011@gmail.com>
2021-01-21 07:29:42 +0530

    open account api setup

---
+++ src/index.js
@@ -0,0 +1,49 @@
+require("./models/User");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const User = mongoose.model("User");
+
+app.use(bodyParser.json());
+
+/*URL TO ACCESS MONGODB */
+const mongoUri =
+  "mongodb+srv://admin:pass@cluster0.e1ifg.mongodb.net/leads_db?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongoDB.com ");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error in connecting to mongo DB ", err);
+});
+
+app.post("/openaccount", async (req, res) => {
+  const { email, contact_number, contactLead_on } = req.body;
+  if (!email || !contact_number)
+    return res.status(422).send({
+      error:
+        "You must provide email id  and contact number for account creation",
+    });
+  try {
+    const user = new User({ email, contact_number, contactLead_on });
+    await user.save();
+    res.send(
+      "Thank you for your interest , to complete account verification process, our team would contact you soon."
+    );
+  } catch (err) {
+    return res.status(422).send({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
+});
---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contact_number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  leadGenerated_on: {
+    type: String,
+    default: Date,
+  },
+  contactLead_on: {
+    type: String,
+    default: Date,
+  },
+});
+
+// user.pre("save", function (next) {
+//   now = new Date();
+//   this.contactLead_on = now;
+//   if (!this.leadGenerated_on) {
+//     this.leadGenerated_on = now;
+//   }
+//   next();
+// });
+
+mongoose.model("User", userSchema);
